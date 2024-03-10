27148b1c0787f2d2d17efec12e8ef9f08f519c8c
Bigbozz47 <116621615+Bigbozz47@users.noreply.github.com>
2024-03-10 17:46:48 +0700

    Delete Mock Up 1 directory

--- Mock Up 1/script.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document.querySelector('.sidebar');
-const togleBtn = document.querySelector('.toggle-btn');
-
-togleBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-});
